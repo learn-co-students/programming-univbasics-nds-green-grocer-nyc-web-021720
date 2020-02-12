5f8d99b799cec7418e86587d8ec39bb105094802
Samantha Ponce <samanthaponce5@gmail.com>
2020-02-12 00:08:33 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,12 +1,13 @@
 def find_item_by_name_in_collection(name, collection)
-  counter = 0 
-  while counter < collection.length 
-  if collection[counter] [:item] == name
-  return collection[counter]
+i = 0 
+while i < collection.length 
+index = collection[i]
+if index[:item] == name 
+  return index
+end 
+i += 1 
 end
-counter+= 1 
-end
-end
+ end
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
@@ -32,58 +33,70 @@ def consolidate_cart(cart)
 end
 new_cart
 end
-
-def apply_coupons(cart, coupons)
-  counter = 0 
-  while counter < coupons.length
-  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-  coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
+  
+  
+  
+  
+  
+  def apply_coupons(cart, coupons)
+  i = 0 
+  while i < coupons.length
+  cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
+  coupon_item_name = "#{coupons[i][:item]} W/COUPON"
   cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
-  if cart_item && cart_item[:count] >= coupons[counter][:num]
+  if cart_item && cart_item[:count] >= coupons[i][:num]
     if cart_item_with_coupon 
-      cart_item_with_coupon[:count] += coupons[counter][:num]
-      cart_item[:count] -= coupons[counter][:num]
+      cart_item_with_coupon[:count] += coupons[i][:num]
+      cart_item[:count] -= coupons[i][:num]
     else
       cart_item_with_coupon = {
         :item => coupon_item_name,
-        :price => coupons[counter][:cost] / coupons[counter][:num],
-        :count => coupons[counter][:num],
+        :price => coupons[i][:cost] / coupons[i][:num],
+        :count => coupons[i][:num],
         :clearance => cart_item[:clearance]
       } 
       cart << cart_item_with_coupon
-      cart_item[:count] -= coupons[counter][:num]
+      cart_item[:count] -= coupons[i][:num]
     end
   end
 
-  counter += 1 
+  i += 1 
 end 
 cart
 end
 
-
 def apply_clearance(cart)
-counter = 0 
-while counter < cart.length 
-if cart[counter][:clearance]
-cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
+i = 0 
+while i < cart.length
+if cart[i][:clearance]
+  cart[i][:price] = (cart[i][:price]-(cart[i][:price]*0.20 )).round(2)
 end
-counter += 1
+i += 1 
 end
 cart
 end
 
 def checkout(cart, coupons)
- consolidated_cart = consolidate_cart(cart)
- couponed_cart = apply_coupons(consolidated_cart, coupons)
- final_cart= apply_clearance(couponed_cart)
- total = 0 
- counter = 0 
- while counter < final_cart.length
- total += final_cart[counter][:price] * final_cart[counter][:count]
- counter += 1 
- end 
-  if total > 100 
-    total -= (total * 0.10)
- end
- total
+  consolidated_cart= consolidate_cart(cart)
+  applied = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(applied)
+  i = 0 
+  total = 0 
+  while i < final_cart.length
+  total += final_cart[i][:price] * final_cart[i][:count]
+   i += 1
 end
+if total > 100
+  total -= (total * 0.10 )
+end
+total
+end
+# Consult README for inputs and outputs
+#
+# This method should call
+# * consolidate_cart
+# * apply_coupons
+# * apply_clearance
+#
+# BEFORE it begins the work of calculating the total (or else you might have
+# some irritated customers
